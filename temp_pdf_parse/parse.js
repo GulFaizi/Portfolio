2f6600280b3f0b507a3f300d84b086b97dc3fa72
Gul Faizi <gulahmadfaizi1987@gmail.com>
2026-02-22 16:20:36 -0500

    feb 22 update.

--- temp_pdf_parse/parse.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const pdf = require('pdf-parse');
-
-let dataBuffer = fs.readFileSync('G:\\Projects\\Portfolio\\client\\src\\assets\\Gul Ahmad Faizi Resume.pdf');
-
-pdf(dataBuffer).then(function(data) {
-    console.log(data.text);
-}).catch(err => console.error(err));
